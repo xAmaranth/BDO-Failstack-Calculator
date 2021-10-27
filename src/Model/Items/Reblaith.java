64ba57563a2d5bc9d1bd7e01b2e342385dccacc9
xAmaranth <tse.edison@gmail.com>
2021-10-26 20:15:27 -0700

    Updated Reblaith class to handle PRI Reblaiths as well. Added image of a PRI Reblaith for use by the GUI.
    Signed-off-by: xAmaranth <tse.edison@gmail.com>

--- src/Model/Items/Reblaith.java
+++ src/Model/Items/Reblaith.java
@@ -1,6 +1,8 @@
 package Model.Items;
 
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Reblaith extends Item{
 
@@ -8,5 +10,17 @@ public class Reblaith extends Item{
         image = new ImageIcon("reblaith.png");
         name = "Reblaith Gloves";
         value = 12900L;
+    }
+
+    public Reblaith(int enhancementLevel, long value){
+        List<ImageIcon> possibleIcons = Arrays.asList(
+                new ImageIcon("reblaith.png"),
+                new ImageIcon("priReblaith.png"));
+        List<String> possibleNames = Arrays.asList("Reblaith Gloves", "PRI Reblaith Gloves");
+
+        image = possibleIcons.get(enhancementLevel);
+        name = possibleNames.get(enhancementLevel);
+
+        this.value = value;
     }
 }
